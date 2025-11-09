af05d362428b4cbba40120686f157da4e31d8b7b
vbetsch <vic.betsch@gmail.com>
2025-11-09 02:00:16 +0100

    refactor: rename variables

--- src/app/usecases/create-game.usecase.ts
+++ src/app/usecases/create-game.usecase.ts
@@ -3,9 +3,9 @@ import { Master } from '@engine/Master/master';
 
 export class CreateGameUseCase {
   public handle(): void {
-    const game = new Game();
-    const master: Master = new Master(game);
-    master.install();
-    master.prepare();
+    const gameCreated = new Game();
+    const gameMaster: Master = new Master(gameCreated);
+    gameMaster.install();
+    gameMaster.prepare();
   }
 }

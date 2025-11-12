1302fd752c8be3f4be47627c01c9b9344a868852
vbetsch <vic.betsch@gmail.com>
2025-11-12 20:16:07 +0100

    feat: improve typing

--- src/app/usecases/create-and-run-game.usecase.ts
+++ src/app/usecases/create-and-run-game.usecase.ts
@@ -4,7 +4,7 @@ import { IUseCase } from '@usecases/abstract/usecase.interface';
 
 export class CreateAndRunGameUseCase implements IUseCase {
   public handle(): void {
-    const gameCreated = new Game();
+    const gameCreated: Game = new Game();
     const gameMaster: Master = new Master(gameCreated);
     gameMaster.install();
     gameMaster.prepare();
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { PlayCommand } from '@commands/play.command';
+import { ICommand } from '@commands/abstract/command.interface';
 
-const playCommand: PlayCommand = new PlayCommand();
+const playCommand: ICommand = new PlayCommand();
 
 console.log('--------- 7 WONDERS DUEL ---------');
 playCommand.execute();

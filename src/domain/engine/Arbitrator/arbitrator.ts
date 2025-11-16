e647552bd0d073f5cb059292d49b8ac7a2569bae
vbetsch <vic.betsch@gmail.com>
2025-11-16 02:11:40 +0100

    feat: add gameStatistics param

--- src/domain/engine/Arbitrator/arbitrator.ts
+++ src/domain/engine/Arbitrator/arbitrator.ts
@@ -1,12 +1,13 @@
 import { inject, injectable } from 'tsyringe';
 import { Rules } from '@engine/Rules/rules';
 import { GameResultType } from '@engine/Game/game-result.type';
+import { GameStatisticsType } from '@engine/Game/game-statistics.type';
 
 @injectable()
 export class Arbitrator {
   public constructor(@inject(Rules) private readonly _rules: Rules) {}
 
-  public getGameResult(): GameResultType {
+  public getGameResult(gameStatistics: GameStatisticsType): GameResultType {
     return {
       winner: 'Player 1',
       loser: 'Player 2',

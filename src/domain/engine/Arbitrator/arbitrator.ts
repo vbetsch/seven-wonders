93c8e86a606e62499ddb61d4d8f17decce9ef50e
vbetsch <vic.betsch@gmail.com>
2025-11-16 03:45:30 +0100

    refactor: comment useless line

--- src/domain/engine/Arbitrator/arbitrator.ts
+++ src/domain/engine/Arbitrator/arbitrator.ts
@@ -1,12 +1,11 @@
-import { inject, injectable } from 'tsyringe';
-import { Rules } from '@engine/Rules/rules';
+import { injectable } from 'tsyringe';
 import { GameResultType } from '@engine/Game/game-result.type';
 import { PlayerStatisticsType } from '@engine/Player/player-statistics.type';
 import { GameStatistics } from '@engine/GameStatistics/game-statistics';
 
 @injectable()
 export class Arbitrator {
-  public constructor(@inject(Rules) private readonly _rules: Rules) {}
+  // public constructor(@inject(Rules) private readonly _rules: Rules) {}
 
   private _getFormattedLosers(
     losersStatistics: PlayerStatisticsType[]

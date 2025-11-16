e8ad388c8064e89661275870e786647990c95ed9
vbetsch <vic.betsch@gmail.com>
2025-11-16 04:11:11 +0100

    feat: add expected logs

--- src/domain/engine/Master/master.ts
+++ src/domain/engine/Master/master.ts
@@ -2,6 +2,9 @@ import { Game } from '@engine/Game/game';
 import { GamePhase } from '@engine/Game/game-phase.enum';
 import { Logger } from '@core/Logger/logger';
 import { container } from 'tsyringe';
+import { PlayerStatisticsType } from '@engine/Player/player-statistics.type';
+import { Arbitrator } from '@engine/Arbitrator/arbitrator';
+import { GameResultType } from '@engine/Game/game-result.type';
 
 export class Master {
   private readonly _logger: Logger;
@@ -33,5 +36,56 @@ export class Master {
   public run(): void {
     this._nextPhase(GamePhase.PREPARING, GamePhase.RUNNING);
     this._logger.log('Running the game...');
+
+    const arbitrator: Arbitrator = container.resolve(Arbitrator);
+    // CASE 1
+    this._logger.log('Arbitrator - CASE 1');
+    const playerOneStatisticsCase1: PlayerStatisticsType = {
+      id: 'Player 1',
+      score: 32,
+    };
+    const playerTwoStatisticsCase1: PlayerStatisticsType = {
+      id: 'Player 2',
+      score: 45,
+    };
+    this._logger.log(
+      `Arbitrator receives ${playerOneStatisticsCase1.score} for ${playerOneStatisticsCase1.id} and ${playerTwoStatisticsCase1.score} for ${playerTwoStatisticsCase1.id}`
+    );
+    const gameResultCase1: GameResultType | null = arbitrator.getGameResult([
+      playerOneStatisticsCase1,
+      playerTwoStatisticsCase1,
+    ]);
+    if (gameResultCase1) {
+      this._logger.log(
+        `Winner=${gameResultCase1.winner}, Loser=${gameResultCase1.losers[0]}`
+      );
+    } else {
+      this._logger.log('Equality ! No winner, no loser');
+    }
+
+    // CASE 2
+    this._logger.log('Arbitrator - CASE 2');
+    const playerOneStatisticsCase2: PlayerStatisticsType = {
+      id: 'Player 1',
+      score: 53,
+    };
+    const playerTwoStatisticsCase2: PlayerStatisticsType = {
+      id: 'Player 2',
+      score: 29,
+    };
+    this._logger.log(
+      `Arbitrator receives ${playerOneStatisticsCase2.score} for ${playerOneStatisticsCase2.id} and ${playerTwoStatisticsCase2.score} for ${playerTwoStatisticsCase2.id}`
+    );
+    const gameResultCase2: GameResultType | null = arbitrator.getGameResult([
+      playerOneStatisticsCase2,
+      playerTwoStatisticsCase2,
+    ]);
+    if (gameResultCase2) {
+      this._logger.log(
+        `Winner=${gameResultCase2.winner}, Loser=${gameResultCase2.losers[0]}`
+      );
+    } else {
+      this._logger.log('Equality ! No winner, no loser');
+    }
   }
 }

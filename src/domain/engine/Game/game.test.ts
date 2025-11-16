582ff78d2ece9e4246aae95fd5c3af560da42fb1
vbetsch <vic.betsch@gmail.com>
2025-11-16 01:12:16 +0100

    refactor: rewrite Game test

--- src/domain/engine/Game/game.test.ts
+++ src/domain/engine/Game/game.test.ts
@@ -2,13 +2,17 @@ import { Game } from './game';
 import { GamePhase } from './game-phase.enum';
 
 describe('Game', () => {
+  let game: Game;
+
+  beforeEach(() => {
+    game = new Game();
+  });
+
   it('should be well implemented', () => {
-    const game: Game = new Game();
     expect(game).toBeDefined();
     expect(game).toBeInstanceOf(Game);
   });
   it('should have a phase', () => {
-    const game: Game = new Game();
     expect(game.phase).toBeDefined();
     expect(game.phase).toStrictEqual(GamePhase.WAITING);
   });

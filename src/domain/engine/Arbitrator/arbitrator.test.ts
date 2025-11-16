128b7155b9a714020f9437314cdf4187a9dfab1f
vbetsch <vic.betsch@gmail.com>
2025-11-16 01:56:36 +0100

    feat: create getGameResult

--- src/domain/engine/Arbitrator/arbitrator.test.ts
+++ src/domain/engine/Arbitrator/arbitrator.test.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import { container } from 'tsyringe';
 import { Arbitrator } from './arbitrator';
+import { GameResultType } from '@engine/Game/game-result.type';
 
 describe('Arbitrator', () => {
   let arbitrator: Arbitrator;
@@ -17,5 +18,13 @@ describe('Arbitrator', () => {
     expect(arbitrator).toBeTruthy();
     expect(arbitrator).toBeInstanceOf(Arbitrator);
     expect(arbitrator).toBeDefined();
+  });
+
+  it('should get game result', () => {
+    const gameResult: GameResultType = {
+      winner: 'Player 1',
+      loser: 'Player 2',
+    };
+    expect(arbitrator.getGameResult()).toStrictEqual(gameResult);
   });
 });
--- src/domain/engine/Arbitrator/arbitrator.ts
+++ src/domain/engine/Arbitrator/arbitrator.ts
@@ -1,7 +1,15 @@
 import { inject, injectable } from 'tsyringe';
 import { Rules } from '@engine/Rules/rules';
+import { GameResultType } from '@engine/Game/game-result.type';
 
 @injectable()
 export class Arbitrator {
   public constructor(@inject(Rules) private readonly _rules: Rules) {}
+
+  public getGameResult(): GameResultType {
+    return {
+      winner: 'Player 1',
+      loser: 'Player 2',
+    };
+  }
 }

342f24ecfc68b3f46fdc29e54eabee443fe324e6
vbetsch <vic.betsch@gmail.com>
2025-11-18 20:39:02 +0100

    docs: add code documentation on rules

--- src/domain/engine/Rules/rules.ts
+++ src/domain/engine/Rules/rules.ts
@@ -2,52 +2,52 @@ import { injectable } from 'tsyringe';
 
 @injectable()
 export class Rules {
-  /**
-   * The number of players (default: 2)
-   */
   // eslint-disable-next-line @typescript-eslint/no-magic-numbers
   private readonly _playersNumber: number = 2;
 
-  /**
-   * The number of ages (default: 3)
-   */
   // eslint-disable-next-line @typescript-eslint/no-magic-numbers
   private readonly _agesNumber: number = 3;
 
-  /**
-   * The total number of wonders available (default: 12)
-   */
   // eslint-disable-next-line @typescript-eslint/no-magic-numbers
   private readonly _availableWondersTotal: number = 12;
 
-  /**
-   * The number of wonders available per player (default: 4)
-   */
   // eslint-disable-next-line @typescript-eslint/no-magic-numbers
   private readonly _availableWondersPerPlayer: number = 4;
 
-  /**
-   * The maximum number of usable wonders (default: 7)
-   */
   // eslint-disable-next-line @typescript-eslint/no-magic-numbers
   private readonly _maxUsedWondersTotal: number = 7;
 
+  /**
+   * The number of players (default: 2)
+   */
   public get playersNumber(): number {
     return this._playersNumber;
   }
 
+  /**
+   * The number of ages (default: 3)
+   */
   public get agesNumber(): number {
     return this._agesNumber;
   }
 
+  /**
+   * The total number of wonders available (default: 12)
+   */
   public get availableWondersTotal(): number {
     return this._availableWondersTotal;
   }
 
+  /**
+   * The maximum number of usable wonders (default: 7)
+   */
   public get maxUsedWondersTotal(): number {
     return this._maxUsedWondersTotal;
   }
 
+  /**
+   * The number of wonders available per player (default: 4)
+   */
   public get availableWondersPerPlayer(): number {
     return this._availableWondersPerPlayer;
   }

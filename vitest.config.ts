e08464066fedd996c7bbbf463169cdbfd62d02eb
vbetsch <vic.betsch@gmail.com>
2026-08-28 20:06:08 +0200

    chore: format and improve vitest config file

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,21 +1,32 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  resolve: {
+    tsconfigPaths: true,
+  },
   test: {
-    environment: 'node',
-    include: ['**/src/**/**/**/*.test.ts'],
     coverage: {
+      exclude: [
+        '*/**.interface.ts',
+        '*/**.type.ts',
+        'index.ts',
+        'node_modules/**',
+        'vitest.config.ts',
+        'vitest-report/**',
+      ],
+      include: ['src/**/*.ts'],
       provider: 'v8',
+      reporter: ['text', 'json', 'html'],
       thresholds: {
         lines: 100,
         functions: 100,
         branches: 100,
         statements: 100,
       },
-      exclude: ['node_modules/**', 'vitest.config.ts', 'vitest-report/**'],
     },
-  },
-  resolve: {
-    tsconfigPaths: true,
+    environment: 'node',
+    exclude: ['dist/**', 'node_modules/**'],
+    globals: true,
+    include: ['src/**/*.test.ts'],
   },
 });

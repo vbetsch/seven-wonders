8e903193b773db520f3e00df68213d1d27560659
vbetsch <vic.betsch@gmail.com>
2026-08-28 16:25:40 +0200

    refactor: use spy for logger in age test

--- src/domain/engine/Age/age.test.ts
+++ src/domain/engine/Age/age.test.ts
@@ -1,18 +1,17 @@
 import 'reflect-metadata';
-import { container } from 'tsyringe';
 import { Logger } from '@core/Logger/logger';
 import { Age } from './age';
 
 describe('Age', () => {
-  let mockLogger: jest.Mocked<Logger>;
+  let loggerLogSpy: jest.SpyInstance;
   let age: Age;
 
   beforeEach(() => {
-    mockLogger = {
-      log: jest.fn(),
-    } as unknown as jest.Mocked<Logger>;
-    container.registerInstance(Logger, mockLogger);
+    loggerLogSpy = jest.spyOn(Logger.prototype, 'log');
     age = new Age(1, 12);
+  });
+  afterEach(() => {
+    loggerLogSpy.mockRestore();
   });
 
   it('should be well implemented', () => {
@@ -31,10 +30,10 @@ describe('Age', () => {
   });
 
   it('should log during instantiation', () => {
-    expect(mockLogger.log).toHaveBeenCalledWith('Age 1 started');
-    expect(mockLogger.log).toHaveBeenCalledWith(
+    expect(loggerLogSpy).toHaveBeenCalledWith('Age 1 started');
+    expect(loggerLogSpy).toHaveBeenCalledWith(
       'There are 12 cards to discover during this age !'
     );
-    expect(mockLogger.log).toHaveBeenCalledWith('Age 1 finished');
+    expect(loggerLogSpy).toHaveBeenCalledWith('Age 1 finished');
   });
 });

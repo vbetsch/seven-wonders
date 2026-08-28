def07cf7c5fbd8829cb9ecf4267cbbb9f14db612
vbetsch <vic.betsch@gmail.com>
2026-08-28 16:08:33 +0200

    test: age logs

--- src/domain/engine/Age/age.test.ts
+++ src/domain/engine/Age/age.test.ts
@@ -1,9 +1,17 @@
+import 'reflect-metadata';
+import { container } from 'tsyringe';
+import { Logger } from '@core/Logger/logger';
 import { Age } from './age';
 
 describe('Age', () => {
+  let mockLogger: jest.Mocked<Logger>;
   let age: Age;
 
   beforeEach(() => {
+    mockLogger = {
+      log: jest.fn(),
+    } as unknown as jest.Mocked<Logger>;
+    container.registerInstance(Logger, mockLogger);
     age = new Age(1, 12);
   });
 
@@ -20,5 +28,13 @@ describe('Age', () => {
   it('should have a number of cards', () => {
     expect(age.cardsNumber).toBeDefined();
     expect(age.cardsNumber).toBe(12);
+  });
+
+  it('should log during instantiation', () => {
+    expect(mockLogger.log).toHaveBeenCalledWith('Age 1 started');
+    expect(mockLogger.log).toHaveBeenCalledWith(
+      'There are 12 cards to discover during this age !'
+    );
+    expect(mockLogger.log).toHaveBeenCalledWith('Age 1 finished');
   });
 });

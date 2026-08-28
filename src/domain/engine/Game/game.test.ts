0ace71f8119cda8a46b58bdb286550b6224c9aab
vbetsch <vic.betsch@gmail.com>
2026-08-28 16:59:12 +0200

    test: game run

--- src/domain/engine/Game/game.test.ts
+++ src/domain/engine/Game/game.test.ts
@@ -1,10 +1,13 @@
 import { Game } from './game';
 import { GamePhase } from './game-phase.enum';
+import { GameLoop } from '@engine/GameLoop/game-loop';
 
 describe('Game', () => {
+  let gameLoopStartSpy: jest.SpyInstance;
   let game: Game;
 
   beforeEach(() => {
+    gameLoopStartSpy = jest.spyOn(GameLoop.prototype, 'start');
     game = new Game();
   });
 
@@ -12,8 +15,14 @@ describe('Game', () => {
     expect(game).toBeDefined();
     expect(game).toBeInstanceOf(Game);
   });
+
   it('should have a phase', () => {
     expect(game.phase).toBeDefined();
     expect(game.phase).toStrictEqual(GamePhase.WAITING);
+  });
+
+  it('should start the loop', () => {
+    game.run();
+    expect(gameLoopStartSpy).toHaveBeenCalledTimes(1);
   });
 });

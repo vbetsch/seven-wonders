5ac0645f13f60c4bd71cab82c05c325079daa2cb
vbetsch <vic.betsch@gmail.com>
2026-08-28 15:53:36 +0200

    test: game loop

--- src/domain/engine/GameLoop/game-loop.test.ts
+++ src/domain/engine/GameLoop/game-loop.test.ts
@@ -1,26 +1,35 @@
 import 'reflect-metadata';
+import { container } from 'tsyringe';
 import { GameLoop } from './game-loop';
 import { Rules } from '@engine/Rules/rules';
-import { container } from 'tsyringe';
+import { Age } from '@engine/Age/age';
+
+jest.mock('@engine/Age/age');
 
 describe('GameLoop', () => {
   let mockRules: jest.Mocked<Rules>;
   let loop: GameLoop;
+  const AgeMock = jest.mocked(Age);
 
   beforeEach(() => {
     mockRules = {
       agesCardsNumber: [42, 38, 54],
     } as unknown as jest.Mocked<Rules>;
     container.registerInstance(Rules, mockRules);
-
     loop = container.resolve(GameLoop);
   });
 
   afterEach(() => {
     container.clearInstances();
+    jest.clearAllMocks();
   });
 
-  it('should run the expected number of ages', () => {
+  it('should instanciate ages', () => {
     loop.start();
+
+    expect(AgeMock).toHaveBeenCalledTimes(3);
+    expect(AgeMock).toHaveBeenNthCalledWith(1, 0, 42);
+    expect(AgeMock).toHaveBeenNthCalledWith(2, 1, 38);
+    expect(AgeMock).toHaveBeenNthCalledWith(3, 2, 54);
   });
 });

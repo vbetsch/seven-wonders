b316ad742163d3f5c594f8fbdbf8430933b1d0e6
vbetsch <vic.betsch@gmail.com>
2026-08-31 20:49:41 +0200

    fix: eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import namingVbetsch from '@vbetsch/config-eslint/naming';
 import prettierVbetsch from '@vbetsch/config-eslint/prettier';
 import strictVbetsch from '@vbetsch/config-eslint/strict';
 import spellcheckVbetsch from '@vbetsch/config-eslint/spellcheck';
+import spellcheck from 'eslint-plugin-spellcheck';
 
 export default [
   ...baseVbetsch,
@@ -11,13 +12,16 @@ export default [
   spellcheckVbetsch,
   ...prettierVbetsch,
 
-  /* IMPORTANT: You have to add these lines only for the strict module ! */
   {
+    /* IMPORTANT: You have to add these lines only for the strict module ! */
     languageOptions: {
       parserOptions: {
         project: './tsconfig.json',
         tsconfigRootDir: import.meta.dirname,
       },
+    },
+    rules: {
+      '@typescript-eslint/no-magic-numbers': 'off',
     },
   },
 
@@ -30,6 +34,18 @@ export default [
           project: './tsconfig.json',
         },
       },
+    },
+  },
+
+  {
+    plugins: { spellcheck },
+    rules: {
+      'spellcheck/spell-checker': [
+        'warn',
+        {
+          skipWords: ['Enum'],
+        },
+      ],
     },
   },
 ];
